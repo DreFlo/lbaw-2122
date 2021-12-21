d52805133c8bd49d8098a3772c1cd5e90aeea8f1
Diogo <up201605360@fe.up.pt>
2021-12-21 21:53:13 +0000

    added new policies

---
+++ app/Policies/FriendshipPolicy.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Friendship;
+use App\Models\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class FriendshipPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Determine whether the user can view any models.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function viewAny(User $user)
+    {
+        return $user->admin_flag;
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Friendship  $friendship
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function view(User $user, Friendship $friendship)
+    {
+        return ($user->id === $friendship->user_1 || $user->id === $friendship->user_2);
+    }
+
+    /**
+     * Determine whether the user can create models.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function create(User $user)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Friendship  $friendship
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function update(User $user, Friendship $friendship)
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Friendship  $friendship
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function delete(User $user, Friendship $friendship)
+    {
+        if ($user->id === $friendship->user_1 || $user->id === $friendship->user_2)
+            return Response::allow();
+        else if($user->admin_flag === true)
+            return Response::allow();
+        
+        return Response::deny();
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Friendship  $friendship
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function restore(User $user, Friendship $friendship)
+    {
+        return $user->admin_flag;
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Friendship  $friendship
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function forceDelete(User $user, Friendship $friendship)
+    {
+        if ($user->id === $friendship->user_1 || $user->id === $friendship->user_2)
+            return Response::allow();
+        else if($user->admin_flag === true)
+            return Response::allow();
+        
+        return Response::deny();
+    }
+}
