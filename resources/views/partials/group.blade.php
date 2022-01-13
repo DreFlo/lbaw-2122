b3cbec8bcb36692ffd4c6cfbef5d966af9c8534b
Tiago Rodrigues <tiaguito.andre21764@gmail.com>
2022-01-13 16:26:29 +0000

    Create Post done!

--- resources/views/partials/group.blade.php
+++ resources/views/partials/group.blade.php
@@ -2,18 +2,22 @@
 <div class="cover_group">
     <img style="width:100%; height:100%" src="{{ url($group->coverPic->path) }}" class="cover_pic_profile" alt="{{ $group->coverPic->alt }}"> </img>
     <div class="name_group">
-        <a href="/groups/{{ $group->id }} " class="font_group">
+        <a href="/groups/{{ $group->id }} " class="font_group_name" >
             {{ $group->name }}
         </a>
     </div>
 </div>
 
 <div class="cont_group">
+
     <div class="posts_group">
+        @include('partials.create_post', ['group' => $group, 'style' => 'width:98%'])
+
         @foreach($group->posts as $post)
             @include('partials.post', ['post' => $post, 'style' => 'width:98%;'])
         @endforeach
     </div>
+
     <div class="members_group">
 
         <table class="table_group_members">
@@ -23,9 +27,13 @@
                 @foreach($rowmembers as $member)
                 <td class="row_group">
                     <a class="font_group" href='/users/{{ $member->id }}'>
+                        
                         <img alt="profile_pic" src="{{url($member->profilePic->path)}}" style="width:150px; height:150px;">
                         </img>
+                        <br>
                         {{ $member->name }}
+                        
+                        
                     </a>
                 </td>
                 @endforeach
