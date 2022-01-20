2222312298139a16f51529b2daa975e38a3bec50
André Flores <andrejesusferflores@gmail.com>
2022-01-20 16:10:46 +0000

    Fix o profile create post

--- resources/views/pages/profile.blade.php
+++ resources/views/pages/profile.blade.php
@@ -58,44 +58,7 @@
                 @method('DELETE')
                 <button class="btn btn-primary" style="background-color: red" type="submit" title="Delete">Delete Account</button>
             </form>
-        </div>
-    @endif
-
-    @if (optional(Auth::user())->id == $user->id)
-        <div class="post-profile">
-            <h1 style="margin-left:10px" class="font_group">Create Post!</h1>
-            <form method="POST" action="{{route('posts.store')}}" enctype="multipart/form-data" id="create_post_form">
-                @csrf
-                <div class="form-group">
-                    <label title="Enter post text" class="create_post_form">
-                        <textarea class="form-control" name="text" placeholder="Enter Text" required></textarea>
-                    </label>
-                </div>
-                    <input type="hidden" name="visibility" value="{{Auth::user()->priv_stat}}">
-                    <lable class="create_post_form">Visibility
-                        @if (auth()->user()->priv_stat === 'Public')
-                            <select name="visibility" id="visibility_selector">
-                                <option value="Public" selected>Public</option>
-                                <option value="Private">Private</option>
-                            </select>
-                        @else
-                            <select name="visibility" id="visibility_selector">
-                                <option value="Public">Public</option>
-                                <option value="Private" selected>Private</option>
-                            </select>
-                        @endif
-                    </lable>
-                <label class="create_post_form">Images
-                    <input type="file" name="images[]" class="form-control" multiple>
-                </label>
-                <label title="Search users to tag" id="tag_label" class="create_post_form">
-                    Tags
-                    <input type="text" class="tag_search_field">
-                    <button type="button" class="btn btn-primary tag_search_button">Search</button>
-                </label>
-                    <input type="hidden" value="{{Auth::user()->id}}" name="user_id">
-                <button type="submit" class="btn btn-primary" style="margin: 1%">Create</button>
-            </form>
+            <button class="btn btn-primary" type="button" onclick="location.href = '{{route('posts.create')}}';" title="Delete">Create Post</button>
         </div>
     @endif
 
